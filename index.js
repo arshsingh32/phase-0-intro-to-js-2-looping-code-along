8757cf11547529e53e13d2759fc4643e20becc99
Arshdeep <arshsingh3223@gmail.com>
2021-05-27 15:29:08 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const name = ['Arsh', 'Diego', 'Nyllie'];
+const event = 'countdown'
+
+function writeCards(name, event) {
+    let newArray = [];
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+
+    return newArray;
+}
+
+writeCards('name', 'event');
+
+function countDown(init = 10) {
+    while (init >=0) {
+        console.log(init--);
+    }
+}
